17b9a2ecd3c4cc13bb670ffe1fddb6710305e1e9
rew08324tr0ijfv <74111565+rew08324tr0ijfv@users.noreply.github.com>
2020-11-07 18:51:27 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa6dccdbadc03001fb0a21f",
+				"5fa6dccdbadc03001fb0a221"
+			]
+		},
+		"5fa6dccdbadc03001fb0a21f": {
+			"id": "5fa6dccdbadc03001fb0a21f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa6dccdbadc03001fb0a221": {
+			"id": "5fa6dccdbadc03001fb0a221",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,85 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { StackItem, Stack, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Text
+				font="--base"
+				text-transform="uppercase"
+				letter-spacing="1px"
+				max-width="850px"
+				color="--grey"
+				margin="0px"
+			>
+				we are
+			</Text>
+			<Text
+				as="h1"
+				margin="8px 0px 32px"
+				font="--headline1"
+				md-font="--headline2"
+				color="--dark"
+				max-width="850px"
+			>
+				the people
+			</Text>
+			<Stack margin-top="auto" color="--grey" font="--base">
+				<StackItem width="50%" md-width="100%">
+					<Text margin="0px">
+						the people are us
+					</Text>
+				</StackItem>
+				<StackItem width="50%" md-width="100%" />
+			</Stack>
+			<Button
+				text-align="center"
+				cursor="progress"
+				box-shadow="0 0 0 0 #606060"
+				transition="all --transitionDuration-normal --transitionTimingFunction-easeInOut 0s"
+				filter="hue-rotate(0deg)"
+			>
+				that's it
+			</Button>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
